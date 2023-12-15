51c97f56bfaf13aeca7bccb728702dde71f2b8b8
sergio1982dev <sergio1982.dev@gmail.com>
2023-12-15 14:01:30 -0300

    Projeto tabuada utilizando ASP.NET Web Application

---
+++ 4_WebFormExercicioTabuada/WebFormTabuada.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormTabuada.aspx.cs" Inherits="_4_WebFormExercicioTabuada.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Bem vindo a página Tabuada</h1>
+            <hr />
+            <h2>Insira valor para cálculo da tabuada de 1 a 10</h2>
+            <h4>Use virgula ',' para valores decimais</h4>
+            <p>
+                <asp:TextBox ID="tbox1" runat="server" placeholder="Insira valor aqui"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+                <asp:Button ID="btn1" runat="server" Text="Exibir tabuada" OnClick="btn1_Click" />
+            &nbsp;&nbsp;
+                <asp:Button ID="btn2" runat="server" OnClick="btn2_Click" Text="Limpar conteúdo" />
+            </p>
+            <p>
+                <asp:ListBox ID="lbox1" runat="server" Rows="10"></asp:ListBox>
+            </p>
+        </div>
+        <hr />
+    </form>
+</body>
+</html>
---
+++ 4_WebFormExercicioTabuada/WebFormTabuada.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _4_WebFormExercicioTabuada
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tbox1.Focus();
+
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+
+            lbox1.Items.Clear();
+
+
+            try
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    double n = i * Convert.ToDouble(tbox1.Text);
+                    string res = i.ToString() + " X " + tbox1.Text + " = " + n.ToString();
+                    lbox1.Items.Add(res);
+                }
+            }
+
+            catch
+            {
+                lbox1.Items.Add("Dado não suportado!");
+                lbox1.Items.Add("Digite um valor numérico");
+            }
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            tbox1.Text = "";
+            lbox1.Items.Clear();
+            tbox1.Focus();
+        }
+    }
+}
